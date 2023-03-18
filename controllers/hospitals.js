e6ff09170787b9d20b6ea262cf0d5f65b2c68354
priesdelly <priesdelly@gmail.com>
2023-03-18 23:35:10 +0700

    release all

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -18,7 +18,7 @@ exports.getHospitals = async (req, res, next) => {
 
   let queryStr = JSON.stringify(reqQuery);
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match=>`$${match}`)
-  query = Hospital.find(JSON.parse(queryStr));
+  query = Hospital.find(JSON.parse(queryStr)).populate('appointments');;
 
   //Select Fields
   if (req.query.select) {
@@ -132,10 +132,11 @@ exports.updateHospital = async (req, res, next) => {
 //@access   Private
 exports.deleteHospital = async (req, res, next) => {
   try {
-    const hospital = await Hospital.findByIdAndDelete(req.params.id);
+    const hospital = await Hospital.findById(req.params.id);
     if (!hospital) {
       return res.status(400).json({ success: false });
     }
+    hospital.remove();
     return res.status(200).json({ success: true, data: {} });
   } catch (err) {
     return res.status(400).json({ success: false });
--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const {
-    getHospitals, getHospital, createHospital, updateHospital, deleteHospital,
+  getHospitals,
+  getHospital,
+  createHospital,
+  updateHospital,
+  deleteHospital,
 } = require('../controllers/hospitals');
+
+//Include other resource routers
+const appointmentRouter = require('./appointments');
+
 const router = express.Router();
 
-const {protect, authorize} = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
-
-router.route('/').get(getHospitals).post(protect, authorize('admin'),
-    createHospital
-);
+//Re-route into other resource routers
+router.use('/:hospitalId/appointments/', appointmentRouter);
 
 router
-    .route('/:id')
-    .get(getHospital)
-    .put(protect, authorize('admin'), updateHospital)
-    .delete(protect, authorize('admin'), deleteHospital);
+  .route('/')
+  .get(getHospitals)
+  .post(protect, authorize('admin'), createHospital);
+
+router
+  .route('/:id')
+  .get(getHospital)
+  .put(protect, authorize('admin'), updateHospital)
+  .delete(protect, authorize('admin'), deleteHospital);
 
 module.exports = router;
