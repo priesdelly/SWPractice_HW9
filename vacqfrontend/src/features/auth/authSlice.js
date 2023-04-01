daab54ccecfbfb7add1cb442e5246e402a962573
priesdelly <priesdelly@gmail.com>
2023-04-01 17:22:30 +0700

    move backend file

---
+++ vacqfrontend/src/features/auth/authSlice.js
@@ -0,0 +1,59 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import authService from './authService';
+
+const user = localStorage.getItem('user');
+const initialState = {
+    user: user ? user : null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: '',
+};
+
+//Register new user
+export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
+    console.log(user);
+    try {
+        return await authService.register(user);
+    }
+    catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+    }
+});
+
+//Login user
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+    console.log(user);
+});
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        reset: (state) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.isSuccess = false;
+            state.message = '';
+        }
+    },
+    extraReducer: (builder) => {
+        builder.addCase(register.pending, (state) => {
+            state.isLoading = true;
+        });
+        builder.addCase(register.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.message = action.payload;
+        });
+        builder.addCase(register.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload;
+            state.user = null
+        });
+    }
+});
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
--- vacqfrontend/src/pages/Register.jsx
+++ vacqfrontend/src/pages/Register.jsx
@@ -1,6 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { FaUser } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { register, reset } from '../features/auth/authSlice'
+
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -8,9 +12,29 @@ function Register() {
         email: '',
         password: '',
         password2: '',
+        role: 'user'
     });
 
-    const { name, email, password, password2 } = formData
+    const { name, email, password, password2, role } = formData
+
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
+    const { user, isLoading, isError, isSuccess, message } = useSelector((state) => { return state.auth });
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+
+        //redirect when user is logged in
+        if (isSuccess || user) {
+            navigate('/');
+        }
+
+        dispatch(reset());
+    }, [isError, isSuccess, user, message, navigate, dispatch]);
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -23,6 +47,15 @@ function Register() {
         e.preventDefault();
         if (password !== password2) {
             toast.error('Passwords do not match');
+        } else {
+            const userData = {
+                name,
+                email,
+                password,
+                role,
+            }
+
+            dispatch(register(userData));
         }
     }
 
@@ -37,27 +70,35 @@ function Register() {
 
             <section className='form'>
                 <form onSubmit={onSubmit}>
+
                     <div className="form-group">
                         <input type="text" className='form-control' id='name' name='name' value={name} onChange={onChange} placeholder='Enter your name' required />
                     </div>
+
                     <div className="form-group">
                         <input type="email" className='form-control' id='email' name='email' value={email} onChange={onChange} placeholder='Enter your email' required />
                     </div>
+
                     <div className="form-group">
                         <input type="password" className='form-control' id='password' name='password' value={password} onChange={onChange} placeholder='Enter your password' required />
                     </div>
+
                     <div className="form-group">
                         <input type="password" className='form-control' id='password2' name='password2' value={password2} onChange={onChange} placeholder='Confirm your password' required />
                     </div>
 
                     <div className="form-group">
+                        <input type="text" className='form-control' id='role' name='role' value={role} onChange={onChange} placeholder='Enter your Role' required />
+                    </div>
+
+                    <div className="form-group">
                         <button className="btn btn-block">Submit</button>
                     </div>
+
                 </form>
             </section>
         </>
     )
-
 }
 
 export default Register;
